710d0638534650148b462fe1f0ee1c220b98f5a0
bhd.m@ya.ru <bhd.m@ya.ru>
2016-02-07 23:40:27 +0300

    add calendar widget

---
+++ src/AppBundle/Controller/WidgetController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use donatj\SimpleCalendar;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class WidgetController extends Controller
+{
+    /**
+     * @Route("/widget-calendar/{year}/{month}", name="widget_calendar", defaults={"year"=null, "month"=null }, options={"exponse" = true})
+     * @Template("")
+     */
+    public function calendarAction($year=null, $month=null){
+        if ($year === null){
+            $year = (new \DateTime())->format('Y');
+        }
+        if ($month === null){
+            $month = (new \DateTime())->format('m');
+        }
+
+        $dateStart = new \DateTime($year.'-'.$month.'-01 00:00:00');
+        $dateEnd = new \DateTime($year.'-'.($month+1).'-01 00:00:00');
+
+        $events = $this->getDoctrine()->getRepository('AppBundle:Event')->findEvent($dateStart,$dateEnd);
+
+        return ['events' => $events];
+    }
+}
