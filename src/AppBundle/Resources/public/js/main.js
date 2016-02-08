70a3653860916696ab6262cff26e67e269bcb2c4
bhd.m@ya.ru <bhd.m@ya.ru>
2016-02-08 03:12:50 +0300

    add carusel

--- src/AppBundle/Resources/public/js/main.js
+++ src/AppBundle/Resources/public/js/main.js
@@ -1,5 +1,23 @@
 $(document).ready(function(){
    $('#login').click(function(){
        $('#loginRegisterModal').modal();
-   })
+   });
+
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:true,
+        navText: ['‹','›'],
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:1
+            },
+            1000:{
+                items:1
+            }
+        }
+    })
 });
