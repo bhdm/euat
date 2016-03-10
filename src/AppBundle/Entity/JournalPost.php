3f7dc2e0489ddea122a0ed54072f107ca4619805
Maxim <bhd.m@ya.ru>
2016-03-10 12:26:33 +0300

    u

--- src/AppBundle/Entity/JournalPost.php
+++ src/AppBundle/Entity/JournalPost.php
@@ -28,12 +28,12 @@ class JournalPost extends BaseEntity{
     protected $title;
 
     /**
-     * @ORM\Column(type="string")
+     * @ORM\Column(type="string", nullable=true)
      */
     protected $titleEn;
 
     /**
-     * @ORM\Column(type="text")
+     * @ORM\Column(type="text", nullable=true)
      */
     protected $descriptionEn;
 
@@ -48,7 +48,7 @@ class JournalPost extends BaseEntity{
     protected $keywordsEn;
 
     /**
-     * @ORM\Column(type="text")
+     * @ORM\Column(type="text", nullable=true)
      */
     protected $bodyEn;
 
