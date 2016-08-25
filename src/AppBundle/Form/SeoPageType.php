d25876b1af63e7ed3909ed8a0057ae1b8d4e69de
bhdm <bhd.m@ya.ru>
2016-08-26 02:26:04 +0300

    u

--- src/AppBundle/Form/SeoPageType.php
+++ src/AppBundle/Form/SeoPageType.php
@@ -27,7 +27,7 @@ class SeoPageType extends AbstractType
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults(array(
-            'data_class' => 'AppBundle\Entity\Page'
+            'data_class' => 'AppBundle\Entity\SeoPage'
         ));
     }
 }
