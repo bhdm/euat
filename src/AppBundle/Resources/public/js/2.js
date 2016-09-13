31319e37e43511ac1b75f426056bd37c2118f765
bhdm <bhd.m@ya.ru>
2016-09-13 15:59:35 +0300

    u

--- src/AppBundle/Resources/public/js/2.js
+++ src/AppBundle/Resources/public/js/2.js
@@ -1,14 +1,14 @@
-$(function(){
-    var links = {
-        'vk' : 'https://vk.com/public73072501',
-        'tw' : 'https://twitter.com/EuatInfo',
-        'ok' : 'http://ok.ru/group/57034519412770',
-        'fb' : 'https://www.facebook.com/euat.org/'
-    };
-    var $elements = $("a[data-key]");
-    for(var i = 0; $elements.length ; i++) {
-        var $element = $elements.eq(i);
-        var key = $element.data("key");
-        $element.attr("href", links[key]);
-    }
-});
+// $(function(){
+//     var links = {
+//         'vk' : 'https://vk.com/public73072501',
+//         'tw' : 'https://twitter.com/EuatInfo',
+//         'ok' : 'http://ok.ru/group/57034519412770',
+//         'fb' : 'https://www.facebook.com/euat.org/'
+//     };
+//     var $elements = $("a[data-key]");
+//     for(var i = 0; $elements.length ; i++) {
+//         var $element = $elements.eq(i);
+//         var key = $element.data("key");
+//         $element.attr("href", links[key]);
+//     }
+// });
