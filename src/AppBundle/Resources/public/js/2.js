572c41f77fe2ab9adef452b424cdbbf090f26b63
bhdm <bhd.m@ya.ru>
2016-09-13 15:55:33 +0300

    u

--- src/AppBundle/Resources/public/js/2.js
+++ src/AppBundle/Resources/public/js/2.js
@@ -1,14 +1,14 @@
 $(function(){
     var links = {
-        'vk' : 'aHR0cHM6Ly92ay5jb20vcHVibGljNzMwNzI1MDE=',
-        'tw' : 'aHR0cHM6Ly90d2l0dGVyLmNvbS9FdWF0SW5mbw==',
-        'ok' : 'aHR0cDovL29rLnJ1L2dyb3VwLzU3MDM0NTE5NDEyNzcw',
-        'fb' : 'aHR0cHM6Ly93d3cuZmFjZWJvb2suY29tL2V1YXQub3JnLw=='
+        'vk' : 'https://vk.com/public73072501',
+        'tw' : 'https://twitter.com/EuatInfo',
+        'ok' : 'http://ok.ru/group/57034519412770',
+        'fb' : 'https://www.facebook.com/euat.org/'
     };
     var $elements = $("a[data-key]");
     for(var i = 0; $elements.length ; i++) {
         var $element = $elements.eq(i);
         var key = $element.data("key");
-        $element.attr("href", Base64.decode(links[key]));
+        $element.attr("href", links[key]);
     }
 });
