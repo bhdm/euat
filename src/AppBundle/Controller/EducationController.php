015ed6681de85e1fc7005b9de0ea6f601b0b017c
bhdm <bhd.m@ya.ru>
2016-10-21 12:34:19 +0300

    u

--- src/AppBundle/Controller/EducationController.php
+++ src/AppBundle/Controller/EducationController.php
@@ -55,8 +55,8 @@ class EducationController extends Controller
         $token = $this->getToken();
         $backLink = 'http://euat.ru/course/'.$id.'/info';
         $secretkey = 'dhi11nubax';
-        $md5 = md5('back_url='.$backLink.'login='.$this->getUser()->getId().'usr_data=get_login'.md5($secretkey.$token));
-        $link = 'http://www.sovetnmo.ru/cgi-bin/unishell?access_token='.$token.'&usr_data=get_login&back_url='.$backLink.'&login=test_user&ssign='.$md5;
+        $md5 = md5('back_url='.$backLink.'login='.$this->getUser()->getId().'usr_data=get_login'.md5($token.$secretkey));
+        $link = 'http://www.sovetnmo.ru/cgi-bin/unishell?access_token='.$token.'&usr_data=get_login&back_url='.$backLink.'&login='.$this->getUser()->getId().'&ssign='.$md5;
 
         return ['course' => $course, 'statusCourse' => $statusCourse, 'recordBook' => $recordBook, 'link' => $link];
     }
